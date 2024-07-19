de8ba868d098c450c2e424ebe9956a5623ae75da
Mohamed Amine KERKOURI <kerkourima@gmail.com>
2024-07-19 17:40:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mlflow-pytorch-exp',
-    version='0.1.0',
+    version='0.0.1',
     description='Create a PyTorch project with MLflow boilerplate.',
     author='Your Name',
     packages=find_packages(),
