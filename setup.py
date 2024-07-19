473a9fd211879827a032e8c0908cc70ed8d59e7a
Mohamed Amine  KERKOURI <amine@or-pri-26g195.prisme.fr>
2024-07-19 18:29:20 +0200

    fix setup function

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup, find_packages
+from .config import PACKAGES 
+
+def get_requirments():
+    with open('requirements.txt') as f:
+        return f.read().splitlines()
 
 setup(
     name='mlflow-pytorch-exp',
     version='0.0.1',
     description='Create a PyTorch project with MLflow boilerplate.',
-    author='Your Name',
+    author='Mohamed Amine KERKOURI',
+    author_email='mohamed.a.kerkouri@gmail.com',
+    license = 'MIT',
     packages=find_packages(),
+    install_requires = PACKAGES,
     entry_points={
         'console_scripts': ['mlflow-pytorch-exp=mlflow_pytorch_exp.train:main'],
     },
