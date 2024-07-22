795d6f16cfbe61fd62383920010a28977861b9c6
Mohamed Amine KERKOURI <kerkourima@gmail.com>
2024-07-22 13:26:03 +0200

    Update setup.py
    Added the package_data argument

--- setup.py
+++ setup.py
@@ -60,6 +60,18 @@ setup(
     packages=find_packages(),
     install_requires = ["mlflow==2.14.3","pynvml==11.5.3" ],
     include_package_data=True,
+    package_data={
+        'mlflow_pytorch_exp': [
+            'templates/checkpoints/*',
+            'templates/config/*',
+            'templates/data/*',
+            'templates/dataloader/*',
+            'templates/losses/*',
+            'templates/metrics/*',
+            'templates/models/*',
+            'templates/utils/*'
+        ]
+    },
     entry_points={
         'console_scripts': ['mlflow-pytorch-exp=mlflow_pytorch_exp.mlflow_pytorch_exp:main'],
     },
